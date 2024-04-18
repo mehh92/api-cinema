4103f8f8013e2ee6396bc8838bc0c37ca6721915
Mehdi Chiche <mehdi@MacBook-Mehdi.local>
2024-04-18 09:48:31 +0200

    Correction du schema

--- src/routes/filmRoutes.ts
+++ src/routes/filmRoutes.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from "express";
 import IFilm, { filmModel } from "../models/Film";
-import IDirector, { directorModel } from "../models/Realisateur";
 
 
 const router = Router();
